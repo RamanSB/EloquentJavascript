7c0989ecd9b32315315441e65c1571d65872c239
Ramandeep Bedi <ramandeepbedi@Ramandeeps-MacBook-Pro.local>
2020-05-07 00:48:31 +0100

    Exercises 3.1 & 3.2 Completed

--- Chapter 3/eloquent-js-ch3-exercises.js
+++ Chapter 3/eloquent-js-ch3-exercises.js
@@ -17,9 +17,54 @@ function calculateMinimum(){
   var v1 = Number(document.getElementById('1st-val').value);
   var v2 = Number(document.getElementById('2nd-val').value);
   let result = min(v1, v2);
-  let pElem = document.getElementById('result-string');
+  let pElem = document.getElementById('result-string-3.1');
+  pElem.innerHTML = '';  //erases result written from previous calculation                                                                                    
   let resultString = `The minimum of ${v1} & ${v2} is: ${result}`;
   let textNode = document.createTextNode(resultString);
   pElem.appendChild(textNode);
 
 };
+
+
+//Ex 3.2
+/*
+Zero is even
+One is odd
+For any other number N, its evenness is the same as N-2
+*/
+
+function isEven(N){
+  let inputNumber = Number(N);
+  if(!(inputNumber == parseInt(inputNumber))){
+    console.log("Input must be a positive integer (a whole number).");
+    return null;
+  }
+  if(N==0){
+    console.log("N=0");
+    return true;
+  }else if(N==1){
+    console.log("N=1");
+    return false;
+  }
+  console.log(`N=${N}`);
+  return isEven(N-2);
+}
+
+function calculateIsEven(){
+  let result = isEven(document.getElementById('input-val').value);
+  let pElem = document.getElementById('result-string-3.2');
+  pElem.innerHTML = ''; //erases result written from previous calculation
+  let textNode = document.createTextNode(result);
+  pElem.appendChild(textNode);
+}
+
+/*
+The issue with the input parameter N=-1 is that on line 49 - we will subtract 2
+resulting in -3, the base cases of N being equal to 0 or 1 will never be met.
+
+The loop will continue adding more recursive calls to the stack soon resulting in
+a stackoverflow error.
+
+In order to fix this: Any negative integer that is inputted can be multipledi by
+-1. This is true as if x is even or odd so is -x.
+*/
