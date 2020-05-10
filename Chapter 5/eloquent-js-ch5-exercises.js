887189e7b798a9ed84b9186237ab6388c0738138
Ramandeep Bedi <ramandeepbedi@Ramandeeps-MacBook-Pro.local>
2020-05-11 00:29:16 +0100

    Complete Exercise 5.1 - Flattening via Reduce method

---
+++ Chapter 5/eloquent-js-ch5-exercises.js
@@ -0,0 +1,11 @@
+//Ex 5.1 - Flattening
+let inputArray = [[2,12,94], [8,1,-4,49,24,13], [38,91, 21, 18, -49, -20, 31]];
+let initialResult=[]
+
+let finalResult = inputArray.reduce((totalResult, item)=>{
+  return totalResult.concat(item);
+}, initialResult);
+
+console.log(`Flattened Array: ${finalResult}`);
+
+//Ex 5.2 - Your Own Loop.
