028b00659f5352fb1d17c9367e3bed8a00262b47
Ramandeep Bedi <ramandeepbedi@Ramandeeps-MacBook-Pro.local>
2020-05-25 14:22:48 +0100

    Chapter 5 exercises complete

--- Chapter 5/eloquent-js-ch5-exercises.js
+++ Chapter 5/eloquent-js-ch5-exercises.js
@@ -18,4 +18,61 @@ function loop(value, testFunction, bodyFunction, updateFunction){
     loop(returnValue, testFunction, bodyFunction, updateFunction);
 }
 
-//Ex 5.3 -  
+//Ex 5.3 - Everything (Arrays - every method)
+//with loop
+function every(array, test){
+    for(let elem of array){
+        if(!test(elem)){
+            return false;
+        }
+    }
+    return true;
+}
+
+//without Loop
+function every(array, test){
+  let mappedArray = array.map(x => test(x));
+  return !mappedArray.includes(false);
+}
+
+console.log(every([1, 3, 5], n => n < 10));
+console.log(every([2, 4, 16], n => n < 10));
+console.log(every([], n => n < 10));
+
+//Ex 5.4 - Dominant direction
+function mapCharCodesToDirection(array){
+
+    function findDirection(code){
+        for(let script of SCRIPTS){
+            for(let range of script.ranges){
+                if(code >= range[0] && code < range[1]){
+                    return script.direction;
+                }
+            }
+        }
+        return `No script matches code: ${code}`;
+    }
+
+    let mappedArray = array.map(findDirection).filter(x => {return x==='ltr' || x=='rtl'});
+    return mappedArray;
+}
+
+function displayResults(initialString, preResults){
+  let numResults = preResults.length;
+  let ltrCount = preResults.filter(x=>x==='ltr').length;
+  let rtlCount = numResults - ltrCount;
+  console.log(`${initialString} | rtl:${100 * rtlCount/numResults}% ltr: ${100 * ltrCount/numResults}%`);
+}
+
+function convertStringToAscii(string){
+    let charValArr = [];
+    for(let idx in string){
+        charValArr.push(string.charCodeAt(idx));
+    }
+    return charValArr;
+}
+
+let testString = "Hey, مساء الخير";
+let charAsciiCodes = convertStringToAscii(testString);
+let preResultsArray = mapCharCodesToDirection(charAsciiCodes);
+displayResults(testString, preResultsArray);
