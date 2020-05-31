443687d0eb98966ea82fafdff6ed74d64fd3367e
Ramandeep Bedi <ramandeepbedi@Ramandeeps-MacBook-Pro.local>
2020-05-31 14:46:30 +0100

    Ex 6.1

--- Chapter 6/eloquent-js-ch6-exercises.js
+++ Chapter 6/eloquent-js-ch6-exercises.js
@@ -1,1 +1,35 @@
-coords = {x: [2,4], y:[3, 10]}
+"use strict"s
+coords = {x: [2,4], y:[3, 10], nDataPoints: 2};
+
+console.log(coords);
+
+function calculateModulus(){
+  let modulus = [];
+  for(let i=0; i<this.nDataPoints; i++){
+    modulus.unshift(this.x[i]**2 + this.y[i]**2);
+  }
+  return modulus;
+}
+
+calculateModulus.call(coords);
+
+
+class Vec{
+
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+  }
+
+  plus(v){
+    return new Vec(this.x+v.x, this.y+v.y);
+  }
+
+  minus(v){
+    return new Vec(this.x-v.x, this.y-v.y);
+  }
+
+  get length(){
+    return Math.sqrt(this.x**2 + this.y**2);
+  }
+}
