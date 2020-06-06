01b7d092cc9ff58a67c80382034500f14548df1b
Ramandeep Bedi <ramandeepbedi@Ramandeeps-MacBook-Pro.local>
2020-06-06 18:21:02 +0100

    Chapter 6 Exercises complete

--- Chapter 6/eloquent-js-ch6-exercises.js
+++ Chapter 6/eloquent-js-ch6-exercises.js
@@ -1,4 +1,5 @@
-"use strict"s
+"use strict"
+//Ex 6.1
 coords = {x: [2,4], y:[3, 10], nDataPoints: 2};
 
 console.log(coords);
@@ -32,4 +33,86 @@ class Vec{
   get length(){
     return Math.sqrt(this.x**2 + this.y**2);
   }
+}
+
+
+
+//Ex 6.2 - Group
+class Group{
+
+
+  constructor(){
+    this.group = [];
+  }
+
+  //item ---> void (side effect)
+  add(item){
+    if(!this.group.includes(item)){
+      this.group.unshift(item);
+    }
+  }
+
+  //item ---> boolean
+  delete(item){
+    if(this.group.includes(item)){
+      let itemIdx = this.group.indexOf(item);
+      if(itemIdx != this.group.length-1){
+        let temp = this.group[arr.length];
+        this.group[itemIdx] = temp;
+      }
+      this.group.pop();
+      return true;
+    }else{
+      return false;
+    }
+  }
+  //item ---> boolean
+  has(item){
+    return this.group.includes(item);
+  }
+
+  static from(iterable){
+    let newGroup = new Group();
+    for(let item of iterable){
+      newGroup.add(item);
+    }
+    return newGroup;
+  }
+
+  [Symbol.iterator](){
+    return new GroupIterator(this);
+  }
+
+}
+
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+
+
+//Ex 6.3 - Iterable groups
+class GroupIterator{
+
+    constructor(group){
+        this.group = group;
+        this.idx = 0;
+    }
+
+    next(){
+        if(this.idx >= this.group.group.length){
+          return {done: true};
+        }
+        else{
+          let newValue = {value: this.group.group[this.idx], done: false};
+          this.idx++;
+          return newValue;
+        }
+    }
+
 }
